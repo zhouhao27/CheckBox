69552efbdbc41a464f304676b238f0fc55e312a3
Zhou Hao <hao.zhou@cognizant.com>
2019-09-10 19:34:52 +0800

    Add Demo

---
+++ CheckBoxDemo/CheckBoxDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CheckBoxDemo
+//
+//  Created by Hao Zhou on 10/9/19.
+//  Copyright © 2019 Hao Zhou. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
